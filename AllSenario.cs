af7b432375348bb9d9a24eef69a0eaf563431c9b
Rohini Jadhav <rohinisj55@gmail.com>
2021-08-31 22:50:41 +0530

    AllStatement

---
+++ AllSenario.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+
+namespace ProgramFile
+{
+    class AllSenario
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int marks = 65;
+            if (marks >= 80)
+            {
+                Console.WriteLine("This is in if statement");
+            }
+            else if (marks >= 60)
+            {
+                Console.WriteLine(" This is in else if statement");
+            }
+            else if (marks >= 40)
+            {
+                Console.WriteLine("This is in else if to statement");
+            }
+            else
+            {
+                Console.WriteLine("Student has failed");
+            }
+            Console.ReadLine();
+        }
+    }
+
+}
