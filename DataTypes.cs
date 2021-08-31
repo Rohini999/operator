b09553db510fbddcce95c4d61f0e7d26483856a9
Rohini Jadhav <rohinisj55@gmail.com>
2021-08-31 22:38:13 +0530

    AllDataTypes

---
+++ DataTypes.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+
+namespace ProgramFile
+{
+    class DataTypes
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            bool boolVar = true;
+            byte byteVar = 25;
+            short shortVar = -556;
+            ushort ushortVar = 96;
+            int intVar = -100;
+            uint uintVar = 2000;
+            long longVar = -569800;
+            ulong ulongVar = 97000;
+            float floatVar = 10.54F;
+            double doubleVar = 146782.57543D;
+            char charVar = 'A';
+            Console.WriteLine("Value Data Types ");
+            Console.WriteLine(boolVar);
+            Console.WriteLine(byteVar);
+            Console.WriteLine(shortVar);
+            Console.WriteLine(ushortVar);
+            Console.WriteLine(intVar);
+            Console.WriteLine(uintVar);
+            Console.WriteLine(longVar);
+            Console.WriteLine(ulongVar);
+            Console.WriteLine(floatVar);
+            Console.WriteLine(doubleVar);
+            Console.WriteLine(charVar);
+        }
+    }
+}
