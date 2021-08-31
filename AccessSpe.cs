9600f195239716feda21bdcc91debb27c0b50890
Rohini Jadhav <rohinisj55@gmail.com>
2021-08-31 22:11:42 +0530

    AccessSpecifier

---
+++ AccessSpe.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ProgramFile
+    
+{
+    class Student
+    {
+        public string name = "Rohini jadhav";
+
+        public void print()
+        {
+            Console.WriteLine(" Student class");
+        }
+    }
+    class Student2
+    {
+        protected string name = "abhi jadhav";
+    }
+
+
+    class Program1 : Student2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Student student1 = new Student();
+            Console.WriteLine("Name: " + student1.name);
+            student1.print();
+            Console.ReadLine();
+
+            Program program1 = new Program();
+            Console.WriteLine("Name: " + program1.name);
+            Console.ReadLine();
+
+        }
+
+    }
+}
