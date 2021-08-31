8c69186daed03e287761e19d3d8174ac0e355467
Rohini Jadhav <rohinisj55@gmail.com>
2021-08-31 22:56:39 +0530

    Break and continue

---
+++ BreakCounti.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+
+namespace ProgramFile
+{
+    class BreakCounti
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            con obj1 = new con();
+            obj1.con1();
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 4)
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
+class con
+{
+    public void con1()
+
+    {
+
+        for (int i = 0; i < 10; i++)
+        {
+            if (i == 4)
+            {
+                continue;
+            }
+            Console.WriteLine(i);
+        }
+    }
+
+}
