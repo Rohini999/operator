ff756474cb2f4f04c7d316d4b39d17f29057b0b5
Rohini Jadhav <rohinisj55@gmail.com>
2021-08-31 20:50:29 +0530

    stringCollection

---
+++ StringCollection.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
+namespace ProgramFile
+{
+    class StringCollection
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("String_Collections");
+            DemoStringCollection obj = new DemoStringCollection();
+            obj.PrintStringCollection();
+
+
+        }
+    }
+
+    class DemoStringCollection
+    {
+        public void PrintStringCollection()
+        {
+            System.Collections.Specialized.StringCollection sc = new System.Collections.Specialized.StringCollection();
+            Console.WriteLine("Befor Adding Elements in String Count");
+            Console.WriteLine(sc.Count);
+
+            // Adding elements in StringCollection
+            sc.Add("C#");
+            sc.Add("C++");
+            sc.Add("Python");
+            sc.Add("Java");
+            sc.Add("SQL");
+
+
+            Console.WriteLine("After Adding Element in String Count");
+            Console.WriteLine(sc.Count);
+        }
+    }
+}
+
