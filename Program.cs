3fa680f004d137944255b7242f418210bd02addd
Rohini Jadhav <rohinisj55@gmail.com>
2021-08-31 14:15:10 +0530

    Calculator

--- Program.cs
+++ Program.cs
@@ -1,210 +1,134 @@
-﻿using System;
-namespace Operator
+﻿//using System;
+//namespace TestConsoleApplication11
+
+using System;
+
+namespace ProgramFile
 {
-    class Program
+    class program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Arithmetic obj = new Arithmetic();
-            obj.arth();
-            Logical obj2 = new Logical();
-            obj2.log();
-            Relational obj3 = new Relational();
-            obj3.rel();
-            Assignment obj4 = new Assignment();
-            obj4.Ass();
-            Bitwise obj5 = new Bitwise();
-            obj5.bit();
-            unary obj6 = new unary();
-            obj6.una();
-            stringop obj7 = new stringop();
-            obj7.str1();
-        }
-        class Arithmetic
-        {
-            public void arth()
+            int a, b;
+            float c;
+            int choice;
+
+            Console.WriteLine(" CALCULATOR  ");
+            Console.WriteLine("CALCULATION ");
+            Console.WriteLine("1. Addition (+)");
+            Console.WriteLine("2. Substraction (-)");
+            Console.WriteLine("3. Multiplication (*)");
+            Console.WriteLine("4. Division (/)");
+            Console.WriteLine("5. EXIT ");
+
+            Console.WriteLine("\nENTER YOUR FIRST NUMBER:- ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("ENTER YOUR SECOND NUMBER:- ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nENTER YOUR CHOICE:- ");
+
+            choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
             {
-
-                int num1 = 20;
-                int num2 = 30;
-                int result;
-                result = num1 + num2;
-                Console.WriteLine("Value is {0}", result);
-                result = num1 - num2;
-                Console.WriteLine("Value is {0}", result);
-                result = num1 * num2;
-                Console.WriteLine("Value is {0}", result);
-                result = num1 / num2;
-                Console.WriteLine("Value is {0}", result);
-                result = num1 % num2;
-                Console.WriteLine("Value is {0}", result);
+                case 1:
+                    c = a + b;
+                    Console.WriteLine("Result Is :- " + c);
+                    break;
+                case 2:
+                    c = a - b;
+                    Console.WriteLine("Result Is :- " + c);
+                    break;
+                case 3:
+                    c = a * b;
+                    Console.WriteLine("Result Is :- " + c);
+                    break;
+                case 4:
+                    c = a / b;
+                    Console.WriteLine("Result Is :- " + c);
+                    break;
+                case 5:
+                    Environment.Exit(5);
+                    break;
             }
-        }
-    }
-
-}
-
-
-class Logical
-{
-    public void log()
-    {
-        int x = 5;
-        Console.WriteLine(x > 3 && x < 10);
-        Console.WriteLine(x > 3 || x < 4);
-        Console.WriteLine(!(x > 3 && x < 10));
-    }
-}
-
-class Relational
-{
-    public void rel()
-    {
-        int a = 21;
-        int b = 10;
-
-        if (a == b)
-        {
-            Console.WriteLine(" a is equal to b");
-        }
-        else
-        {
-            Console.WriteLine(" a is not equal to b");
-        }
-
-        if (a < b)
-        {
-            Console.WriteLine(" a is less than b");
-        }
-        else
-        {
-            Console.WriteLine(" a is not less than b");
-        }
-        if (a > b)
-        {
-            Console.WriteLine("  a is greater than b");
-        }
-        else
-        {
-            Console.WriteLine(" a is not greater than b");
-        }
-
-
-        a = 5;
-        b = 20;
-
-        if (a <= b)
-        {
-            Console.WriteLine("a is either less than or equal to  b");
-        }
-
-        if (b >= a)
-        {
-            Console.WriteLine("b is either greater than or equal to b");
+            Console.ReadLine();
         }
     }
 }
 
 
-class Assignment
-{
-    public void Ass()
-    {
-        int x = 5;
-        Console.WriteLine(x);
 
-        x += 3;
-        Console.WriteLine(x);
-
-        x -= 3;
-        Console.WriteLine(x);
-
-        x *= 3;
-        Console.WriteLine(x);
-
-        x /= 3;
-        Console.WriteLine(x);
-
-        x &= 3;
-        Console.WriteLine(x);
-
-        x |= 3;
-        Console.WriteLine(x);
-    }
-}
-
-
-class Bitwise
-{
-    public void bit()
-    {
-        int a = 60;
-        int b = 13;
-        int c = 0;
-
-        c = a & b;
-        Console.WriteLine("Value of c is {0}", c);
-
-        c = a | b;
-        Console.WriteLine("Value of c is {0}", c);
-
-        c = a ^ b;
-        Console.WriteLine("Value of c is {0}", c);
-
-        c = ~a;
-        Console.WriteLine(" Value of c is {0}", c);
-
-        c = a << 2;
-        Console.WriteLine("Value of c is {0}", c);
-
-        c = a >> 2;
-        Console.WriteLine("Value of c is {0}", c);
-        Console.ReadLine();
-    }
-}
-
-class unary
-{
-    public void una()
-    {
-        int x = 23;
-        int result;
-
-        Console.WriteLine("x is " + x);
-        result = x++;
-        Console.WriteLine("Post increment of x is " + x);
-
-        Console.WriteLine("x is " + x);
-        result = x--;
-        Console.WriteLine("Post decrement of x is " + x);
-
-        //Console.WriteLine("x is " + x);
-        result = ++x;
-        Console.WriteLine("Pre increment of x is " + x);
-
-        Console.WriteLine("x is " + x);
-        result = --x;
-        Console.WriteLine("Pre decrement of x is " + x);
-    }
-}
-
-class stringop
-{
-    public void str1()
-    {
-        string s1 = "my";
-        string s2 = "Welcome";
-        string s3 = "home";
-        bool result1, result2;
-
-
-        result1 = s1 == s2;
-        Console.WriteLine("s1 is equal to s2: {0} ", result1);
-
-        result2 = s1 == s3;
-        Console.WriteLine("s1 is equal to s3: {0} ", result2);
-    }
-}
+//{
+//    class program
+//{
+//    static void Main(string[] args)
+//    {
+//        Console.WriteLine("Enter the action to be performed");
+//        Console.WriteLine("Press 1 for Addition");
+//        Console.WriteLine("Press 2 for Subtraction");
+//        Console.WriteLine("Press 3 for Multiplication");
+//        Console.WriteLine("Press 4 for Division \n");
+//        int action = Convert.ToInt32(Console.ReadLine());
+//        Console.WriteLine("Enter 1st input");
+//        int input_1 = Convert.ToInt32(Console.ReadLine());
+//        Console.WriteLine("Enter 2nd input");
+//        int input_2 = Convert.ToInt32(Console.ReadLine());
+//        int result = 0;
+//        switch (action)
+//        {
+//            case 1:
+//                {
+//                    result = Addition(input_1, input_2);
+//                    break;
+//                }
+//            case 2:
+//                {
+//                    result = Subtraction(input_1, input_2);
+//                    break;
+//                }
+//            case 3:
+//                {
+//                    result = Multiplication(input_1, input_2);
+//                    break;
+//                }
+//            case 4:
+//                {
+//                    result = Division(input_1, input_2);
+//                    break;
+//                }
+//            default:
+//                Console.WriteLine("Wrong action! try again");
+//                break;
+//        }
+//        Console.WriteLine("The result is {0}", result);
+//        Console.ReadKey();
+//    }
+//    //Addition  
+//    public static int Addition(int input_1, int input_2)
+//    {
+//        int result = input_1 + input_2;
+//        return result;
+//    }
+//    //Substractionn  
+//    public static int Subtraction(int input_1, int input_2)
+//    {
+//        int result = input_1 + input_2;
+//        return result;
+//    }
+//    //Multiplication  
+//    public static int Multiplication(int input_1, int input_2)
+//    {
+//        int result = input_1 + input_2;
+//        return result;
+//    }
+//    //Division  
+//    public static int Division(int input_1, int input_2)
+//    {
+//        int result = input_1 + input_2;
+//        return result;
+//    }
+//}
+//}
 
 
