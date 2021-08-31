be241b1b8f43b3da5922e2fca144edf3654ff37f
Rohini Jadhav <rohinisj55@gmail.com>
2021-08-31 23:39:40 +0530

    Nested

---
+++ Nested.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+
+namespace ProgramFile
+{
+    public class Outer_class
+    {
+        public void method1()
+        {
+            Console.WriteLine("Outer class method");
+        }
+        public class Inner_class
+        {
+            public void method2()
+            {
+                Console.WriteLine("Inner class Method");
+            }
+        }
+    }
+
+
+    public class G1
+    {
+
+
+        static public void Main()
+        {
+
+
+            Outer_class obj1 = new Outer_class();
+            obj1.method1();
+
+            Outer_class.Inner_class obj2 = new Outer_class.Inner_class();
+
+            obj2.method2();
+        }
+    }
+}
