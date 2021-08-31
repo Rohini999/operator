5637cfea9152945be1483e0a33b34dca8e962b47
Rohini Jadhav <rohinisj55@gmail.com>
2021-08-31 23:17:17 +0530

    factorial of number inputted by user

---
+++ Fact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ProgramFile
+{
+    class Fact
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------------------------------");
+            int i, fact = 1, number;
+            Console.Write("Enter any Number: ");
+            number = int.Parse(Console.ReadLine());
+            for (i = 1; i <= number; i++)
+            {
+                fact = fact * i;
+            }
+            Console.Write("Factorial of " + number + " is: " + fact);
+            Console.WriteLine("--------------------------------");
+        }
+    }
+}
