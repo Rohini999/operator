0f05a7cda03b924c27cb1a78bc3d7859ad1827ad
Rohini Jadhav <rohinisj55@gmail.com>
2021-08-31 23:25:01 +0530

    ICloneable

---
+++ ICloneable.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+
+namespace ProgramFile
+{
+    class Room : ICloneable
+    {
+        int width;
+
+        public Room(int width)
+        {
+            this.width = width;
+        }
+
+        public object Clone()
+        {
+            return new Room(this.width);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Width of Room = {0}", this.width);
+        }
+    }
+
+
+    class Cloneable
+    {
+        static void Main()
+        {
+            Room roomOne = new Room(1999);
+            Room roomTwo = roomOne.Clone() as Room;
+
+            Console.WriteLine("{0}mm", roomOne);
+            Console.WriteLine("{0}mm", roomTwo);
+
+
+
+        }
+    }
+}
