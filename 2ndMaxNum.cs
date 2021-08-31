b5c2b7fbbb443a5cdb3350fbebf0e5d7acae42bd
Rohini Jadhav <rohinisj55@gmail.com>
2021-08-31 23:01:55 +0530

    Second Maximum Array

---
+++ 2ndMaxNum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ProgramFile
+{
+    class 2ndMaxNum
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] array = { 12, 15, 5, 7, 9, 98, 3, 83, 55 };
+            Array.Sort(array);
+            Array.Reverse(array);
+            Console.WriteLine("Second max Value In Array " + array[1]);
+
+            foreach (var result in array)
+            {
+                Console.Write(result + " ");
+            }
+        }
+    }
+
+}
