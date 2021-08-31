c2ded749e72dc4d398e83f7aab659300480033bb
Rohini Jadhav <rohinisj55@gmail.com>
2021-08-31 23:34:14 +0530

    IComparable

---
+++ IComparable.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+
+namespace ProgramFile
+{
+    class IComparable
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Color[] colors = new Color[]
+   {
+        new Color()
+        {
+            Name = "Red"
+        }, new Color()
+        {
+            Name = "Green"
+        }, new Color()
+        {
+            Name = "Pink"
+        }
+   };
+            Array.Sort(Colors);
+            Array.ForEach(Colors, x => Console.WriteLine(x.Name));
+        }
+
+
+        public class Color : IComparable
+        {
+            public string Name
+            {
+                get;
+                set;
+            }
+            public int MaxSpeed
+            {
+                get;
+                set;
+            }
+            public int CompareTo(object obj)
+            {
+                if (!(obj is Color))
+                {
+                    throw new ArgumentException("Compared Object is not of Color");
+                }
+                Color car = obj as Color;
+                return Name.CompareTo(Color.Name);
+            }
+        }
+    }
+
+}
